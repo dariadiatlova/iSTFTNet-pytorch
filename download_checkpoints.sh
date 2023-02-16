4b43e52c2d9b2e8c0aed168a004ad325a93c0ba8
dariadiatlova <dari.diatlova@gmail.com>
2023-02-16 19:13:49 +0300

    added files to downloaded zip

--- download_checkpoints.sh
+++ download_checkpoints.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cur="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)" # to make running the script from anywhere available
 cd $cur
-mkdir src/data
-cd src/data
+cd src
 # download awesome_checkpoints.zip (generator weights, discriminator weights, onnx models)
-gdown https://drive.google.com/uc?id=1rewyoyuB4QtHL_rb6xOHpOumyU87riHP;
+gdown https://drive.google.com/uc?id=1CQkS0AZyI_e8L68qf_cR3lNRWorspP3B;
 unzip -qq awesome_checkpoints.zip;
