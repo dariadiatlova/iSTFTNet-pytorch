dbc36a064091215a6d641f86ad9b9535fe511d1c
dariadiatlova <dari.diatlova@gmail.com>
2023-02-17 15:09:43 +0300

    updated zip name

--- download_checkpoints.sh
+++ download_checkpoints.sh
@@ -2,6 +2,6 @@
 cur="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)" # to make running the script from anywhere available
 cd $cur
 cd src
-# download awesome_checkpoints.zip (generator weights, discriminator weights, onnx models)
+# download data.zip (awesome_checkpoints - model checkpoints, deep_voices_wav, deep_voices_mel)
 gdown https://drive.google.com/uc?id=1CQkS0AZyI_e8L68qf_cR3lNRWorspP3B;
-unzip -qq awesome_checkpoints.zip;
+unzip -qq data.zip;
